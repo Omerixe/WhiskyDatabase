b79bf35bf508f2e9920af0d75726221ad3cf6217
Jasmin <jasmin@omerixe.ch>
2024-09-07 17:47:04 +0200

    Change styling to more whisky like colours

--- src/App.js
+++ src/App.js
@@ -11,13 +11,81 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { light } from '@mui/material/styles/createPalette';
+
+const deepBrown = '#2C1B18';
+const warmGold = '#A67B5B';
+const white = '#FFFFFF';
+
+const lightGold = '#D4B484';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: deepBrown, 
+    },
+    secondary: {
+      main: warmGold, 
+    },
+    background: {
+      default: '#E1DAD3', // Soft neutral background
+      paper: white, // White card background
+    },
+    text: {
+      primary: deepBrown, 
+      secondary: warmGold, 
+    },
+  },
+  typography: {
+    fontFamily: 'Arial, sans-serif',
+  },
+  components: {
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          backgroundColor: white, 
+          '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            borderColor: warmGold, 
+          },
+          '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: lightGold, 
+          },
+          '& .MuiOutlinedInput-notchedOutline': {
+            borderColor: deepBrown, 
+          },
+        },
+      },
+    },
+    MuiAutocomplete: {
+      styleOverrides: {
+        root: {
+          backgroundColor: white, 
+          borderColor: deepBrown, 
+          borderRadius: '5px',
+        },
+        listbox: {
+          backgroundColor: white, 
+          color: deepBrown, 
+        },
+        option: {
+          backgroundColor: white, 
+          color: deepBrown, 
+          '&:hover': {
+            backgroundColor: warmGold, 
+          },
+        },
+      },
+    },
+  },
+});
 
 const AppContent = () => {
   const { currentUser, logout } = useAuth();
 
   return (
     <>
-      <AppBar position="static">
+      <AppBar position="static" color="primary">
         <Toolbar>
           <Typography variant="h6" style={{ flexGrow: 1 }}>
             Whisky Datenbank
@@ -33,7 +101,8 @@ const AppContent = () => {
           )}
         </Toolbar>
       </AppBar>
-      <Container style={{marginTop: '20px', marginBottom: '20px'}}>
+      
+      <Container style={{ marginTop: '20px', marginBottom: '20px' }}>
         <Routes>
           <Route path="/" element={currentUser ? <WhiskyList /> : <Login />} />
           <Route path="/add" element={currentUser ? <AddWhisky /> : <Login />} />
@@ -49,9 +118,12 @@ const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <div translate='no'>
-          <AppContent />
-        </div>
+        <ThemeProvider theme={theme}>
+          <CssBaseline /> {/* Resets browser's default styles */}
+          <div translate="no">
+            <AppContent />
+          </div>
+        </ThemeProvider>
       </Router>
     </AuthProvider>
   );
